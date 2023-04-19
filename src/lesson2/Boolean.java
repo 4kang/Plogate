1f7a7211e0403dabd9814ce7c32342972a0a8199
kono mayu <konotori08@gmail.com>
2023-04-19 15:56:33 +0900

    Second Commit

---
+++ src/lesson2/Boolean.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//真偽値・比較演算子
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println(6+2 == 5);
+		System.out.println(6+2 != 5);
+
+	}
+
+}
