416e9c627bc70102b79014e4828b09159e73d208
kono mayu <konotori08@gmail.com>
2023-05-10 21:11:43 +0900

    Second Commit

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		hello();
 		strRail();
 		strMain();
-		strName1("なのか");
-		strName2("丹恒");
+		strName1("なのか",300);
+		strName2("丹恒",500);
 		
 	}
 	
@@ -25,12 +25,13 @@ public class Main {
 		System.out.println("主人公はやべぇやつ");
 	}
 	
-	public static void strName1(String name) {
-		System.out.println(name + "さんは氷属性");
+	public static void strName1(String name,int hp) {
+		System.out.println(name + "さんのHPは" + hp);
 	}
 	
-	public static void strName2(String name) {
+	public static void strName2(String name,int hp) {
 		System.out.println(name + "さんは風属性");
+		System.out.println("HPは" + hp);
 	}
 
 }
